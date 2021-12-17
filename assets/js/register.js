7d94c64c54e4e2b3f691421b6fffd51fd53e773c
kasi-antony <antokasi44@gmail.com>
2021-12-17 17:25:19 +0530

    added some changes

--- assets/js/register.js
+++ assets/js/register.js
@@ -56,6 +56,7 @@ function register() {
     array.push(userDetails);
     const accountdetails = JSON.stringify(array);
     localStorage.setItem("USERDETAIL",accountdetails);
+    
     window.location.href="log in.html"
 
     console.table(userDetails);
